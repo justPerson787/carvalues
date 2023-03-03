c0e5d964b13820c406f82c712b63c2ae2ec9c9e1
justPerson787 <n.n.ushakova@gmail.com>
2023-03-03 13:24:26 -0500

    Add config variable for cookie key

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,9 +40,12 @@ const cookieSession = require('cookie-session');
 ],
 })
 export class AppModule {
+  constructor(
+    private configService: ConfigService
+  ) {}
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(cookieSession({
-      keys: ['asdkhgvb6'],
+      keys: [this.configService.get('COOKIE_KEY')],
     }))
     .forRoutes('*');
   }
